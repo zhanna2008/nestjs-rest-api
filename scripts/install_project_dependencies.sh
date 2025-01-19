d5d4cac682a5a6bfa1a2dc4b6d206c162858f829
Zhanna Kurochkina <zhanna_kurochkina@epam.com>
2025-01-19 23:46:09 +0200

    change region

--- scripts/install_project_dependencies.sh
+++ scripts/install_project_dependencies.sh
@@ -16,7 +16,7 @@ npm install --legacy-peer-deps
 # Use AWS Secrets service for setting env variables in prod;
 file_location=./.env
 cat >$file_location <<EOF
-REGION="eu-central-1"
+REGION="us-east-1"
 TABLE_AUTOCREATE="true"
 TABLE_AUTOUPDATE="false"
 DEVELOPMENT="false"
